02f0f37e7e2ed821da659f672339e66d44c1a9a0
krishmirani-crypto <krishmirani08@gmail.com>
2025-10-05 12:57:34 -0400

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,22 @@
+name: 'Penguin'
+age = 15
+is_student = True 
+weight = 90.78
+print ('Name: ', name)
+print ('Data type of name is', type(name))
+print ('Age:', age)
+print ('Data type of age is', type(age))
+print ('Is student: ', is_student )
+print ('Data type of is_student is', type(is_student))
+print ('Weight: ', weight)
+print ('Data type of weight is', type(weight))
+print ('\n After type casting')
+age = str(age)
+is_student = str(is_student)
+weight = int(weight)
+print ('Age: ', age )
+print ('Data type of age is ', type(age))
+print ('Is student:', is_student)
+print ('Data type of is_student is', type(is_student))
+print ('Weight: ', weight)
+print ('Data type of weight', type(weight))
