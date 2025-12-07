3669c9d40c9e9ebd4b30be341638ecc7be9b7b03
krishmirani-crypto <krishmirani08@gmail.com>
2025-12-07 12:43:28 -0400

    Add files via upload

---
+++ reversethestring.py
@@ -0,0 +1,10 @@
+string = input("Please enter your own string : ")
+
+string2 = ('')
+#loop for printing in reverse
+for i in string:
+    string2 = i + string2
+
+print("\nThe Originial String = ", string)
+print("The Reversed String = ", string2)
+
---
+++ sumofwholenumbers.py
@@ -0,0 +1,18 @@
+n = int(input("Enter the number whose sum you want to find: "))
+sum = 0
+for i in range(1, n+1):
+    sum = sum + 1 
+    print("\n Sum = ", sum)
+
+
+Sum = 1
+
+Sum = 3 
+
+Sum = 6
+
+Sum = 10
+
+Sum = 15
+
+Sum = 21
